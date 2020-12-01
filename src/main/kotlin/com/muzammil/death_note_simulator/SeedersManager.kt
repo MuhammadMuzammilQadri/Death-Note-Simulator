36dc9f885126f51df33eb16e0d6909ad8d77f810
Muhammad Muzammil <muhammadmuzammilqadri@live.com>
2020-12-01 18:30:40 +0500

    Changed from h2 memory database to file h2 file database

---
+++ src/main/kotlin/com/muzammil/death_note_simulator/SeedersManager.kt
@@ -0,0 +1,48 @@
+package com.muzammil.death_note_simulator
+
+import com.muzammil.death_note_simulator.models.DeathNote
+import com.muzammil.death_note_simulator.models.Person
+import com.muzammil.death_note_simulator.services.deathnote.IDeathNoteService
+import com.muzammil.death_note_simulator.services.owner.IOwnerService
+import com.muzammil.death_note_simulator.services.person.IPersonService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+/**
+ * Created by Muzammil on 12/1/20.
+ */
+@Component
+class SeedersManager {
+  
+  @Autowired
+  lateinit var deathNoteService: IDeathNoteService
+  
+  @Autowired
+  lateinit var personService: IPersonService
+  
+  @Autowired
+  lateinit var ownerService: IOwnerService
+  
+  val logger = logger()
+  
+  @PostConstruct
+  fun seed() {
+    val person = personService.savePerson(Person(name = "Bad Guy"))
+    val owner = createOwner()
+    logger.warn("Initialized seeders...")
+  }
+  
+  
+  private fun createOwner(): Person {
+    val deathNote = createDeathNote()
+    val owner = personService.savePerson(Person(name = "Light Yagami"))
+    ownerService.makeOwner(deathNote.id!!, owner.id!!)
+    return owner
+  }
+  
+  private fun createDeathNote(): DeathNote {
+    return deathNoteService.createOrUpdateNotebook(DeathNote(name = "Ryuk Notebook"))
+  }
+  
+}
---
+++ src/main/kotlin/com/muzammil/death_note_simulator/Util.kt
@@ -0,0 +1,15 @@
+package com.muzammil.death_note_simulator
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Created by Muzammil on 11/14/20.
+ */
+
+inline fun <reified T> T.logger(): Logger {
+  if (T::class.isCompanion) {
+    return LoggerFactory.getLogger(T::class.java.enclosingClass)
+  }
+  return LoggerFactory.getLogger(T::class.java)
+}
